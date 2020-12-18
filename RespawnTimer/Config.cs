c9fab85175c8d00a8e48f5b37d46c2d566980a68
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2020-12-18 20:42:18 +0100

    Add files via upload

---
+++ RespawnTimer/Config.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RespawnTimer
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Should a timer be only shown, when a spawnning sequence has begun? (NTF Helicopter / Chaos Car arrives)")]
+        public bool ShowTimerOnlyOnSpawn { get; set; } = false;
+
+        [Description("Should the NTF and CI respawn tickets be shown?")]
+        public bool ShowTickets { get; set; } = true;
+
+        [Description("All of the texts in this plugin. If you need to translate them, you can easily do this: (The dashed line is just a interspace. Things in { } are numeric variables, you can for example bold them. DO NOT CHANGE NAMES IN { }!")]
+        public List<string> translations { get; set; } = new List<string>
+        {
+            "<color=orange>You will respawn in: </color>",
+            "You will spawn as: ",
+            "<color=blue>Nine-Tailed Fox</color>",
+            "<color=green>Chaos Insurgency</color>",
+            "<color=blue>NTF Tickets: </color>",
+            "<color=green>CI Tickets: </color>",
+            "-------------------------------------",
+            "<b>{seconds} s</b>",
+            "{ntf_tickets_num}",
+            "{ci_tickets_num}"
+        };
+    }
+}
---
+++ RespawnTimer/Handler.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System.Collections.Generic;
+using MEC;
+using UnityEngine;
+using Respawning;
+
+using EPlayer = Exiled.API.Features.Player;
+
+
+namespace RespawnTimer
+{
+    class Handler
+    {
+        public List<CoroutineHandle> coroutines = new List<CoroutineHandle>();
+
+        StringBuilder text = new StringBuilder();
+
+        public void OnRoundStart()
+        {
+            foreach (CoroutineHandle coroutine in coroutines)
+            {
+                Timing.KillCoroutines(coroutine);
+            }
+
+            coroutines.Add(Timing.RunCoroutine(Timer()));
+            Log.Info("RespawnTimer coroutine started successfully!");
+        }
+
+        private IEnumerator<float> Timer()
+        {
+            while (Round.IsStarted)
+            {
+                yield return Timing.WaitForSeconds(1f);
+
+                if (RespawnManager.Singleton.NextKnownTeam == SpawnableTeamType.None && RespawnTimer.Instance.Config.ShowTimerOnlyOnSpawn) continue;
+
+                text.Clear();
+
+                int i = Mathf.RoundToInt(RespawnManager.Singleton._timeForNextSequence - (float)RespawnManager.Singleton._stopwatch.Elapsed.TotalSeconds);
+
+                text.Append(RespawnTimer.Instance.Config.translations[0] + "\n");
+                text.Append(RespawnTimer.Instance.Config.translations[7] + "\n");
+                text.Replace("{seconds}", i.ToString());
+
+                if (RespawnManager.Singleton.NextKnownTeam != SpawnableTeamType.None)
+                {
+                    text.Append(RespawnTimer.Instance.Config.translations[1]);
+
+                    switch (RespawnManager.Singleton.NextKnownTeam)
+                    {
+                        case SpawnableTeamType.NineTailedFox: text.Append(RespawnTimer.Instance.Config.translations[2]); break;
+                        case SpawnableTeamType.ChaosInsurgency: text.Append(RespawnTimer.Instance.Config.translations[3]); break;
+                    }
+                }
+
+                if(RespawnTimer.Instance.Config.ShowTickets)
+                {
+                    text.Append($"\n\n\n\n\n\n\n\n\n\n\n\n\n\n<align=right>{RespawnTimer.Instance.Config.translations[4]} {RespawnTimer.Instance.Config.translations[8]}</align>" +
+                                $"\n<align=right>{RespawnTimer.Instance.Config.translations[5]} {Respawning.RespawnTickets.Singleton.GetAvailableTickets(Respawning.SpawnableTeamType.ChaosInsurgency)}</align>");
+
+                    text.Replace("{ntf_tickets_num}", Respawning.RespawnTickets.Singleton.GetAvailableTickets(Respawning.SpawnableTeamType.NineTailedFox).ToString());
+                    text.Replace("{ci_tickets_num}", Respawning.RespawnTickets.Singleton.GetAvailableTickets(Respawning.SpawnableTeamType.ChaosInsurgency).ToString());
+                }
+
+                foreach (EPlayer ply in EPlayer.List)
+                {
+                    if (ply.Team == Team.RIP)
+                    {
+                        ply.ShowHint(text.ToString(), 1f);
+                    }
+                }
+            }
+        }
+    }
+}
+
