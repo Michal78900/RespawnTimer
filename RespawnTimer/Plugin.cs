7a2cf424ad4eaaad43914bc74e2760585adf9cbb
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2020-12-18 20:44:54 +0100

    Add files via upload

---
+++ RespawnTimer/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+using ServerEvent = Exiled.Events.Handlers.Server;
+
+namespace RespawnTimer
+{
+    public class RespawnTimer : Plugin<Config>
+    {
+        private static readonly Lazy<RespawnTimer> LazyInstance = new Lazy<RespawnTimer>(() => new RespawnTimer());
+        public static RespawnTimer Instance => LazyInstance.Value;
+
+        public override PluginPriority Priority => PluginPriority.Medium;
+
+        public override string Author => "Michal78900";
+        public override Version Version => new Version(1, 0, 0);
+
+        private RespawnTimer() { }
+
+        private Handler handler;
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            handler = new Handler();
+
+            ServerEvent.RoundStarted += handler.OnRoundStart;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            ServerEvent.RoundStarted -= handler.OnRoundStart;
+
+            handler = null;
+        }
+    }
+}
