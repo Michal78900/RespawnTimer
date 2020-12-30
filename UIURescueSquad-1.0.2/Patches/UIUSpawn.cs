05f021c9f434c9b34d9d3fb01ca19ae2a8c2ce57
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2020-12-30 21:18:04 +0100

    Add files via upload

---
+++ UIURescueSquad-1.0.2/Patches/UIUSpawn.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace UIURescueSquad.Patches
+{
+    [HarmonyPatch(typeof(Respawning.RespawnTickets), nameof(Respawning.RespawnTickets.DrawRandomTeam))]
+    class UIUSpawn
+    {
+        public static void Postfix(ref Respawning.SpawnableTeamType __result)
+        {
+            if (__result == Respawning.SpawnableTeamType.NineTailedFox)
+            {
+                UIURescueSquad.Instance.EventHandlers.IsSpawnable();
+            }
+        }
+    }
+}
