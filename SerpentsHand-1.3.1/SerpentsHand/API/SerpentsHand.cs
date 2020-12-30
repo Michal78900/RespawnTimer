6b4b16297ecf7b946b3bb7ada877586a21768313
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2020-12-30 21:09:14 +0100

    Modified version of SH

---
+++ SerpentsHand-1.3.1/SerpentsHand/API/SerpentsHand.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SerpentsHand.API
+{
+	public static class SerpentsHand
+	{
+		public static void Spawnable()
+		{
+			EventHandlers.IsSpawnable();
+		}
+
+		public static void SpawnPlayer(Player player, bool full = true)
+		{
+			EventHandlers.SpawnPlayer(player, full);
+		}
+
+		public static void SpawnSquad(List<Player> playerList)
+		{
+			EventHandlers.SpawnSquad(playerList);
+		}
+
+		public static void SpawnSquad(int size)
+		{
+			EventHandlers.CreateSquad(size);
+		}
+
+		public static List<Player> GetSHPlayers()
+		{
+			return EventHandlers.shPlayers.Select(x => Player.Get(x)).ToList();
+		}
+	}
+}
