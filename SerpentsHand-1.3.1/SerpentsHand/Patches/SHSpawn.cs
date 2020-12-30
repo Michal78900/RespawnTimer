985b531e974458f7e44afbcd9ea1639fdb384285
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2020-12-30 21:13:47 +0100

    Add files via upload

---
+++ SerpentsHand-1.3.1/SerpentsHand/Patches/SHSpawn.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace SerpentsHand.Patches
+{
+    [HarmonyPatch(typeof(Respawning.RespawnTickets), nameof(Respawning.RespawnTickets.DrawRandomTeam))]
+    class SHSpawn
+    {
+        public static void Postfix(ref Respawning.SpawnableTeamType __result)
+        {
+            if(__result == Respawning.SpawnableTeamType.ChaosInsurgency)
+            {
+                EventHandlers.IsSpawnable();
+            }
+        }
+    }
+}
---
+++ SerpentsHand-1.3.1/SerpentsHand/Patches/Scp939Attack.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SerpentsHand.Patches
+{
+	[HarmonyPatch(typeof(Scp939PlayerScript), nameof(Scp939PlayerScript.CallCmdShoot))]
+	class Scp939Attack
+	{
+		public static void Postfix(Scp939PlayerScript __instance, GameObject target)
+		{
+			Player player = Player.Get(target);
+			if (player.Role == RoleType.Tutorial && !SerpentsHand.instance.Config.FriendlyFire)
+			{
+				player.ReferenceHub.playerEffectsController.DisableEffect<CustomPlayerEffects.Amnesia>();
+			}
+		}
+	}
+}
