1e556e9eb248bb2f09b9327b8f9dd20dd5ebdceb
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2021-02-14 15:50:53 +0100

    Add files via upload

---
+++ SerpentsHandModified/SerpentsHand/Patches/SHSpawn.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace SerpentsHand.Patches
+{
+    [HarmonyPatch(typeof(Respawning.RespawnTickets), nameof(Respawning.RespawnTickets.DrawRandomTeam))]
+    class UIUSpawn
+    {
+        public static void Postfix(ref Respawning.SpawnableTeamType __result)
+        {
+            SerpentsHand.instance.EventHandlers.CalculateChance();
+        }
+    }
+}
