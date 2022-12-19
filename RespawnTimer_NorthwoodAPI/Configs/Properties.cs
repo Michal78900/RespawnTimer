41d3575a2ceb41a129e7db16751675f5b638d3fe
Michal78900 <Michal78900@users.noreply.github.com>
2022-12-19 01:03:32 +0100

    Added properties descriptions

--- RespawnTimer_NorthwoodAPI/Configs/Properties.cs
+++ RespawnTimer_NorthwoodAPI/Configs/Properties.cs
@@ -1,15 +1,22 @@
 ﻿namespace RespawnTimer_NorthwoodAPI.Configs
 {
+    using System.ComponentModel;
+
     public sealed class Properties
     {
+        [Description("Whether the leading zeros should be added in minutes and seconds if number is less than 10.")]
         public bool LeadingZeros { get; private set; } = true;
         
+        [Description("Whether the timer should add time offset depending on MTF/CI spawn.")]
         public bool TimerOffset { get; private set; } = true;
 
+        [Description("How often custom hints should be changed (in seconds).")]
         public int HintInterval { get; private set; } = 10;
         
+        [Description("The Nine-Tailed Fox display name.")]
         public string Ntf { get; private set; } = "<color=blue>Nine-Tailed Fox</color>";
         
+        [Description("The Chaos Insurgency display name.")]
         public string Ci { get; private set; } = "<color=green>Chaos Insurgency</color>";
         
         /*
