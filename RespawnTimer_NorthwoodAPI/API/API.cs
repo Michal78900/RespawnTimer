2fef671b40264955f1439abebfe7f8805c213e99
Michal78900 <Michal78900@users.noreply.github.com>
2024-01-01 19:04:32 +0100

    Yeet this

--- RespawnTimer_NorthwoodAPI/API/API.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RespawnTimer_NorthwoodAPI.API
-{
-    using System.Collections.Generic;
-
-    public static class API
-    {
-        public static List<string> TimerHidden { get; } = new();
-    }
-}
